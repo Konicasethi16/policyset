d35a30f8ae5d704134894c08bf862b4852ce31bf
Konicasethi16 <167297988+Konicasethi16@users.noreply.github.com>
2024-08-13 12:21:29 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,4 @@
+policy "deny-public-rdp-acl-rules" {
+  source = "https://registry.terraform.io/v2/policies/hashicorp/aws-networking-terraform/1.0.2/policy/deny-public-rdp-acl-rules.sentinel?checksum=sha256:b301e80bd612809aeeac263b5d4b5b22b9f84190da67216cc298a96652301086"
+  enforcement_level = "advisory"
+}
